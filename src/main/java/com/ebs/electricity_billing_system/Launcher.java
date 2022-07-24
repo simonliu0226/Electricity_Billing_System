216f184fd45332d3c4ac2ea117136c072ed56217
sliupp <simonliu123456789@gmail.com>
2022-07-23 21:25:27 -0700

    Add files via upload

---
+++ src/main/java/com/ebs/electricity_billing_system/Launcher.java
@@ -0,0 +1,10 @@
+package com.ebs.electricity_billing_system;
+
+/**
+ * @author Simon
+ */
+public class Launcher {
+    public static void main(String[] args) {
+        Start.main(args);
+    }
+}
